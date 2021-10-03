a75e2f29c29c5e0149953a509764d4e08d325e3c
Elvis Guerrero <91610542+ElvisGuerrero@users.noreply.github.com>
2021-10-03 20:43:09 -0300

    version para abrir en python

---
+++ proyecto.py
@@ -0,0 +1,126 @@
+#Desafio 1, Unidad 1 
+#Alumnos: Davida Mansilla y Elvis Guerrero
+#Profesores: Gabriel Ñunez y Luz Cardona
+
+from random import randint 
+
+A1=randint(1,4)
+A2=randint(1,4)
+A3=randint(1,4)
+A4=randint(1,4)
+B1=randint(1,4)
+B2=randint(1,4)
+B3=randint(1,4)
+B4=randint(1,4)
+C1=randint(1,4)
+C2=randint(1,4)
+C3=randint(1,4)
+C4=randint(1,4)
+D1=randint(1,4)
+D2=randint(1,4)
+D3=randint(1,4)
+D4=randint(1,4)
+
+if A1==1 :
+    A1="P"
+if A1==2 or A1==3:
+    A1="B"
+if A1==4:
+    A1="A"
+if A2==1 :
+    A2="P"
+if A2==2 or A2==3:
+    A2="B"
+if A2==4:
+    A2="A"
+if A3==1 :
+    A3="P"
+if A3==2 or A3==3:
+    A3="B"
+if A3==4:
+    A3="A"
+if A4==1 :
+    A4="P"
+if A4==2 or A4==3:
+    A4="B"
+if A4==4:
+    A4="A"
+if B1==1 :
+    B1="P"
+if B1==2 or B1==3:
+    B1="B"
+if B1==4:
+    B1="A"
+if B2==1 :
+    B2="P"
+if B2==2 or B2==3:
+    B2="B"
+if B2==4:
+    B2="A"
+if B3==1 :
+    B3="P"
+if B3==2 or B3==3:
+    B3="B"
+if B3==4:
+    B3="A"
+if B4==1 :
+    B4="P"
+if B4==2 or B4==3:
+    B4="B"
+if B4==4:
+    B4="A"
+if C1==1 :
+    C1="P"
+if C1==2 or C1==3:
+    C1="B"
+if C1==4:
+    C1="A"
+if C2==1 :
+    C2="P"
+if C2==2 or C2==3:
+    C2="B"
+if C2==4:
+    C2="A"
+if C3==1 :
+    C3="P"
+if C3==2 or C3==3:
+    C3="B"
+if C3==4:
+    C3="A"
+if C4==1 :
+    C4="P"
+if C4==2 or C4==3:
+    C4="B"
+if C4==4:
+    C4="A"
+if D1==1 :
+    D1="P"
+if D1==2 or D1==3:
+    D1="B"
+if D1==4:
+    D1="A"
+if D2==1 :
+    D2="P"
+if D2==2 or D2==3:
+    D2="B"
+if D2==4:
+    D2="A"
+if D3==1 :
+    D3="P"
+if D3==2 or D3==3:
+    D3="B"
+if D3==4:
+    D3="A"
+if D4==1 :
+    D4="P"
+if D4==2 or D4==3:
+    D4="B"
+if D4==4:
+    D4="A"
+print("------------------")
+print("|",A1, "|",B1, "|",C1, "|",D1, "|")
+print("|",A2, "|",B2, "|",C2, "|",D2, "|")
+print("|",A3, "|",B3, "|",C3, "|",D3, "|")
+print("|",A4, "|",B4, "|",C4, "|",D4, "|")
+print("------------------")
+ 
