e20704479dc732c34fa512d820d2f893237469d7
Elvis Guerrero <91610542+ElvisGuerrero@users.noreply.github.com>
2021-10-11 18:03:57 -0300

    Codigo finalizado en clases  el 07/10/21
    Este código presenta ya los primeros 5 pasos finalizados, faltando solo unos pequeños detalles

---
+++ Avance_2_07_10_21_Listo.py
@@ -0,0 +1,270 @@
+#Desafio 1, Unidad 1 
+#Alumnos: Davida Mansilla y Elvis Guerrero
+#Profesores: Gabriel Ñunez y Luz Cardona
+
+
+from random import randint 
+
+############
+temp = randint(1,4)
+if temp ==1 :
+    A1="P"
+if temp ==2 or temp ==3:
+    A1="B"
+if temp ==4:
+    A1="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    A2="P"
+if temp ==2 or temp ==3:
+    A2="B"
+if temp ==4:
+    A2="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    A3="P"
+if temp ==2 or temp ==3:
+    A3="B"
+if temp ==4:
+    A3="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    A4="P"
+if temp ==2 or temp ==3:
+    A4="B"
+if temp ==4:
+    A4="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    B1="P"
+if temp ==2 or temp ==3:
+    B1="B"
+if temp ==4:
+    B1="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    B2="P"
+if temp ==2 or temp ==3:
+    B2="B"
+if temp ==4:
+    B2="A"
+############
+temp = randint(1,4)
+if temp==1 :
+    B3="P"
+if temp ==2 or temp ==3:
+    B3="B"
+if temp==4:
+    B3="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    B4="P"
+if temp ==2 or temp ==3:
+    B4="B"
+if temp ==4:
+    B4="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    C1="P"
+if temp ==2 or temp==3:
+    C1="B"
+if temp ==4:
+    C1="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    C2="P"
+if temp or temp ==3:
+    C2="B"
+if temp ==4:
+    C2="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    C3="P"
+if temp ==2 or temp ==3:
+    C3="B"
+if temp ==4:
+    C3="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    C4="P"
+if temp ==2 or temp ==3:
+    C4="B"
+if temp ==4:
+    C4="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    D1="P"
+if temp ==2 or temp ==3:
+    D1="B"
+if temp ==4:
+    D1="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    D2="P"
+if temp ==2 or temp ==3:
+    D2="B"
+if temp ==4:
+    D2="A"
+############
+temp = randint(1,4)
+if temp ==1 :
+    D3="P"
+if temp ==2 or temp ==3:
+    D3="B"
+if temp ==4:
+    D3="A"
+############
+temp == randint(1,4)
+if temp ==1 :
+    D4="P"
+if temp ==2 or temp ==3:
+    D4="B"
+if temp==4:
+    D4="A"
+
+#### Variable elementos ####
+
+ConteoB = 0
+ConteoP = 0
+ConteoA = 0 
+
+#### Variables para conteo ####
+if A1 == "P":
+    ConteoP = ConteoP + 1
+elif A1 == "B": 
+    ConteoB = ConteoB + 1
+elif A1 == "A": 
+    ConteoA = ConteoA + 1
+if A2 == "P": 
+    ConteoP = ConteoP + 1
+elif A2 == "B":
+    ConteoB = ConteoB + 1
+elif A2 == "A":
+    ConteoA = ConteoA + 1
+if A3 == "P":
+    ConteoP = ConteoP + 1
+elif A3 == "B": 
+    ConteoB = ConteoB + 1
+elif A3 == "A": 
+    ConteoA = ConteoA + 1
+if A4 == "P":
+    ConteoP = ConteoP + 1
+elif A4 == "B": 
+    ConteoB = ConteoB + 1
+elif A4 == "A": 
+    ConteoA = ConteoA + 1
+if B1 == "P":
+    ConteoP = ConteoP + 1
+elif B1 == "B": 
+    ConteoB = ConteoB + 1
+elif B1 == "A": 
+    ConteoA = ConteoA + 1
+if B2 == "P": 
+    ConteoP = ConteoP + 1
+elif B2 == "B":
+    ConteoB = ConteoB + 1
+elif B2 == "A":
+    ConteoA = ConteoA + 1
+if B3 == "P":
+    ConteoP = ConteoP + 1
+elif B3 == "B": 
+    ConteoB = ConteoB + 1
+elif B3 == "A": 
+    ConteoA = ConteoA + 1
+if B4 == "P":
+    ConteoP = ConteoP + 1
+elif B4 == "B": 
+    ConteoB = ConteoB + 1
+elif B4 == "A": 
+    ConteoA = ConteoA + 1
+if C1 == "P":
+    ConteoP = ConteoP + 1
+elif C1 == "B": 
+    ConteoB = ConteoB + 1
+elif C1 == "A": 
+    ConteoA = ConteoA + 1
+if C2 == "P": 
+    ConteoP = ConteoP + 1
+elif C2 == "B":
+    ConteoB = ConteoB + 1
+elif C2 == "A":
+    ConteoA = ConteoA + 1
+if C3 == "P":
+    ConteoP = ConteoP + 1
+elif C3 == "B": 
+    ConteoB = ConteoB + 1
+elif C3 == "A": 
+    ConteoA = ConteoA + 1
+if C4 == "P":
+    ConteoP = ConteoP + 1
+elif C4 == "B": 
+    ConteoB = ConteoB + 1
+elif C4 == "A": 
+    ConteoA = ConteoA + 1
+if D1 == "P":
+    ConteoP = ConteoP + 1
+elif D1 == "B": 
+    ConteoB = ConteoB + 1
+elif D1 == "A": 
+    ConteoA = ConteoA + 1
+if D2 == "P": 
+    ConteoP = ConteoP + 1
+elif D2 == "B":
+    ConteoB = ConteoB + 1
+elif D2 == "A":
+    ConteoA = ConteoA + 1
+if D3 == "P":
+    ConteoP = ConteoP + 1
+elif D3 == "B": 
+    ConteoB = ConteoB + 1
+elif D3 == "A": 
+    ConteoA = ConteoA + 1
+if D4 == "P":
+    ConteoP = ConteoP + 1
+elif D4 == "B": 
+    ConteoB = ConteoB + 1
+elif D4 == "A": 
+    ConteoA = ConteoA + 1
+    
+
+#### Porcentaje de ocurriencias ####
+
+PorcP = (ConteoP/16) *100
+PorcB = (ConteoB/16) *100
+PorcA = (ConteoA/16) *100
+
+######################################
+
+print("------------------")
+print("|",A1, "|",B1, "|",C1, "|",D1, "|")
+print("|",A2, "|",B2, "|",C2, "|",D2, "|")
+print("|",A3, "|",B3, "|",C3, "|",D3, "|")
+print("|",A4, "|",B4, "|",C4, "|",D4, "|")
+print("------------------")
+    
+######################################
+
+print("El porcentaje de ocurrencia de las plantas es: ", PorcP)
+print("El porcentaje de ocurrencia de las bacterias es: ",PorcB)
+print("El porcentaje de ocurrencia del agua es: ",PorcA)
+
+##### En caso de mimsa ocurrencia u mayor ocurrencia #####
+
+if PorcP == PorcB :
+    print("El porcentaje de ocurrencias de las dos especias es la misma :) ")
+elif PorcP > PorcB : 
+    print("El porcentaje con mayor de ocurrencias es: ", PorcP)
+elif PorcP < PorcB :
+    print("El porcentaje con mayor ocurrencuias es : ", PorcB)
